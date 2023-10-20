e3a180845ad34ad06163816e1adf5eb9b9b0f82a
HenryTudor8 <126116630+HenryTudor8@users.noreply.github.com>
2023-10-20 12:15:18 -0400

    Movement and Colliders

--- My project/Assets/Player.cs
+++ My project/Assets/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
 
     private Vector3 moveDelta;
 
+    private RaycastHit2D hit;
+
     private void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
@@ -16,11 +18,39 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         //Reset moveDelta
-        moveDelta = Vector3.zero;
+       
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        Debug.Log(x);
-        Debug.Log(y);
+        moveDelta = new Vector3 (x, y, 0); 
+
+        // swap sprite direction if you are going left or right
+
+        if(moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if(moveDelta.x  <0)
+        {  transform.localScale = new Vector3(-1,1, 1);
+        }
+        // making sure we can move in this direction by casting a box and checking if we can move in that direction if we get null
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if(hit.collider == null)
+        {
+            // Movement
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Movement
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+
+
+
+
     }
 }

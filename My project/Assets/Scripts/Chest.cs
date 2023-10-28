08d06ebdd90fa2aeeb481806d11633e109814880
HenryTudor8 <126116630+HenryTudor8@users.noreply.github.com>
2023-10-28 14:59:37 -0400

    New Scene, collectibles

---
+++ My project/Assets/Scripts/Chest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : Collectible
+{
+    public Sprite emptyChest;
+    public int MoneyAmount = 5;
+    protected override void OnCollect()
+    {
+        if(!collected)
+        {
+            collected = true;
+            GetComponent<SpriteRenderer>().sprite = emptyChest;
+            Debug.Log("Grant " + MoneyAmount + "money!");
+        }
+    }
+}

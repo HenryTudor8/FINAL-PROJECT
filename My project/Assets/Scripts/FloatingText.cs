e5c97a72a3222366fc850d0912450f6234475aed
HenryTudor8 <126116630+HenryTudor8@users.noreply.github.com>
2023-11-04 19:52:46 -0400

    scene progression

--- My project/Assets/Scripts/FloatingText.cs
+++ My project/Assets/Scripts/FloatingText.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
-public class FloatingText : MonoBehaviour
+public class FloatingText
 {
     public bool active;
     public GameObject go;
@@ -10,4 +9,35 @@ public class FloatingText : MonoBehaviour
     public Vector3 motion;
     public float duration;
     public float lastShown;
+
+    public void Show()
+    {
+        active = true;
+        lastShown = Time.time;
+        go.SetActive(active);
+    }
+
+    public void Hide()
+    {
+        active = false;
+        go.SetActive(active);
+    }
+
+    public void UpdateFloatingText()
+    {
+        if (!active)
+            return;
+
+
+        // 10 - 7 > 2
+        if (Time.time - lastShown > duration)
+        {
+            Hide();
+        }
+        go.transform.position += motion * Time.deltaTime;
+    }
 }
+
+
+
+
